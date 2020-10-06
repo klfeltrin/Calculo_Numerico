70d53df0bf8c0bd0dda09d28e0c2f22ece3042bd
klfeltrin <kelvin.delucca@gmail.com>
2020-10-06 00:44:23 -0300

    Organizacao inicial do projeto

--- Gauss/script.js
+++ Gauss/script.js
@@ -1,37 +1,71 @@
-function verificar(){
+var res = document.getElementById("res")
+var a
+var b
 
-    if(document.getElementById('txt-entrada').value === ""){
-        window.alert('[ERRO] Preencha os valores da matriz! ')
+function verificar(){
+    if(document.getElementById('txt-entradaA').value === "" ||
+       document.getElementById('txt-entradaB').value === ""){
+        window.alert('[ERRO] Preencha todos os campos! ')
     }
     else{
-        var entrada = document.getElementById('txt-entrada').value
-        verificarEntrada(entrada)
-        gerarMatriz(entrada)
+        var entradaA = document.getElementById('txt-entradaA').value
+        var entradaB = document.getElementById('txt-entradaB').value
+        verificarEntrada(entradaA)
+        verificarEntrada(entradaB)
+        a = gerarMatriz(entradaA)
+        b = gerarMatriz(entradaB)
+        exibirMatriz(a, b)
     }
 }
 
+//Adiciona os valores de entrada nas variaveis
 function gerarMatriz(entrada){
-    var colunas, linhas = []
+    var arrayEntrada = []
+    var colunas = []
+    var linhas = []
+    var tamanho = tamanhoMatriz(entrada)
+    var auxNumero = ""
 
-    for(var i = 0; i < entrada.length; i++){
-        if(entrada[i] != '\n'){
-            console.log('true')
+    for(var i = 0; i <= entrada.length; i++){
+        if(!isNaN(parseInt(entrada[i]))){
+            auxNumero += entrada[i]
+        }
+        else if(i === entrada.length - 1){
+            auxNumero += entrada[i]
+            arrayEntrada.push(parseInt(auxNumero))
+            auxNumero = ""
         }
         else{
-            console.log('false')
-        }
+            arrayEntrada.push(parseInt(auxNumero))
+            auxNumero = ""
+        }        
     }
-    /*
-    for(var i = 0; i < entrada.length; i++){
-        while(entrada[i] != '\n'){
-            if(!isNaN(entrada[i])){
-                linhas.push(entrada[i])
-            }
+    for(var i = 0; i < arrayEntrada.length; i++){
+        auxLinhas = []
+        for(var j = 0; j < tamanho; j++){
+            auxLinhas.push(arrayEntrada[i])
+            i++
         }
-        colunas.push(linhas)
+        linhas.push(auxLinhas)
+        auxLinhas = []
+        i--
     }
+    colunas.push(linhas)
     console.log(colunas)
-    */
+    return colunas
+}
+
+function exibirMatriz(a, b){
+    res.innerHTML += '<br>'
+    for(var i = 0; i < a.length; i++){
+        for(var j = 0; j < a[i].length; j++){
+            res.innerHTML += a[0][i][j] + ' '
+        }
+        for(var j = 0; j < b[i].length; j++){
+            res.innerHTML += ' | ' + b[0][i][j]
+        }
+        res.innerHTML += '<br>'
+    }
 }
 
 //Verifica se os valores de entrada são apenas números
@@ -46,7 +80,20 @@ function verificarEntrada(entrada){
     }
 }
 
+//Verifica o tamanho da matriz
+function tamanhoMatriz(entrada){
+    var count = 0
+    for(var i = 0; i < entrada.length; i++){
+        if(entrada[i] === '\n'){
+            count++
+        }
+    }
+    return count + 1
+}
+
 //Limpa os valores da caixa de texto
 function limpar(){
-    document.getElementById('txt-entrada').value = ""
+    document.getElementById('txt-entradaA').value = "";
+    document.getElementById('txt-entradaB').value = "";
+    res.innerHTML = ""
 }
